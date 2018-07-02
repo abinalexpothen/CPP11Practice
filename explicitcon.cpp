5510712087ea12211f869d187e490af829a9fb48
Abin AlexPothen <abinalexpothen@gmail.com>
2018-07-02 15:18:06 -0400

    explicit constructor

--- explicitcon.cpp
+++ explicitcon.cpp
@@ -6,7 +6,7 @@ struct{
 class Date{
 	int d, m, y;
 public:
-	Date(int dd = today.d, int mm = today.m, int yy=today.y);
+	explicit Date(int dd = 0, int mm = 0, int yy=0);
 };
 
 Date::Date(int dd, int mm, int yy)
@@ -23,9 +23,15 @@ void my_fct(Date d)
 
 void f()
 {
-	Date d {15};	// plausible: becomes {15,today.m,today.y}
-	my_fct(15);	// obscure
-	d = 15;	// obscure
+	Date d1 {15};	// plausible: becomes {15,today.m,today.y}
+	Date d2 = Date{15};	// 
+	//Date d3 = {15}; // error: explicit constructor defined here
+	// Date d4 = 15;  // error: no viable conversion from int to Date
+	// my_fct(15);	// error: candidate function not viable, no 
+	// explicit conversion from int to Date
+	// my_fct({15}); // error: candidate function not viable, cannot convert
+	// initializer list argument to 'Date'
+	my_fct(Date{15});
 }
 // This is obscure as there's no logical connection between 
 // 15 and Date independently of the intricacies of our code
