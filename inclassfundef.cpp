c065b5b73d81b5390ca104a3595d3895a3d38adb
Abin AlexPothen <abinalexpothen@gmail.com>
2018-07-08 09:27:08 -0400

    in class definition

--- inclassfundef.cpp
+++ inclassfundef.cpp
@@ -21,6 +21,8 @@ inline void Date1::add_month(int n) // add n months
 {
 	m+=n;	// increment month
 }
+// not the best definition (needs data validation), but 
+// does the job in making me understand the idea.
 // inline definition keeps class definition simple and 
 // easy to read. Provides textual separation of class's
 // interface and implementation detail.
