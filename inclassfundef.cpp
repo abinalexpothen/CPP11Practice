77429974aa0c4bec10301cc12cc280c655c1f72a
Abin AlexPothen <abinalexpothen@gmail.com>
2018-07-08 09:23:58 -0400

    inline demo

--- inclassfundef.cpp
+++ inclassfundef.cpp
@@ -8,6 +8,22 @@ public:
 private:
 	int d, m, y;
 };
+// function and data member declarations are order independent
+// an alternate definition
+class Date1{
+public:
+	void add_month(int n);
+private:
+	int d, m, y;
+};
+
+inline void Date1::add_month(int n) // add n months
+{
+	m+=n;	// increment month
+}
+// inline definition keeps class definition simple and 
+// easy to read. Provides textual separation of class's
+// interface and implementation detail.
 
 int main()
 {
