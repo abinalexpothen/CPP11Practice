174f4fdb435d2f72edd4af2a2116c349251550ee
Abin AlexPothen <abinalexpothen@gmail.com>
2018-07-08 09:09:30 -0400

    in-class function definition

---
+++ inclassfundef.cpp
@@ -0,0 +1,15 @@
+// In-class function definitions
+// used for small, rarely modified, frequently used func
+
+class Date{
+public:
+	void add_month(int n){m+=n;}	// increment Date's m
+	// ...
+private:
+	int d, m, y;
+};
+
+int main()
+{
+	return 0;
+}
